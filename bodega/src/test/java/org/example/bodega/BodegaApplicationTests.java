6ccfd39c841a8200f88f8fe56023d3caa6b90520
Andeison <an.belandria@duocuc.cl>
2026-05-17 14:13:03 -0400

    correcciones en el trabajo como agregar el eureka, separar las entidades por trabajos individuales en el mismo archivo, bodega listo, sucursal avanzando

---
+++ bodega/src/test/java/org/example/bodega/BodegaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bodega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
