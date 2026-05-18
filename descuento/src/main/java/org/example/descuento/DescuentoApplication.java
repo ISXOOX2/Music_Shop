648f199fc9702d8efcdf6fc2c371003956e954cc
Andeison <an.belandria@duocuc.cl>
2026-05-18 15:39:53 -0400

    edite los puertos duplicados y algunos nombres y desactive el flyway

---
+++ descuento/src/main/java/org/example/descuento/DescuentoApplication.java
@@ -0,0 +1,13 @@
+package org.example.descuento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescuentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DescuentoApplication.class, args);
+    }
+
+}
