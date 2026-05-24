20c6aadeb8c2b3f9dc85fceb5a09e95ade9ff11a
Andeison <an.belandria@duocuc.cl>
2026-05-24 18:37:34 -0400

    agregue el registro de eureka, creacion de copia local de eureka, y para que se registre con ip propia en eureka, tambien modifiqué nombres en los properties y desactive el flyway, voy a empezar a manejar las excepciones globales

--- descuento/src/main/java/org/example/descuento/DescuentoApplication.java
+++ descuento/src/main/java/org/example/descuento/DescuentoApplication.java
@@ -2,8 +2,10 @@ package org.example.descuento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DescuentoApplication {
 
     public static void main(String[] args) {
--- empleado/src/main/java/org/example/empleado/EmpleadoApplication.java
+++ empleado/src/main/java/org/example/empleado/EmpleadoApplication.java
@@ -2,8 +2,10 @@ package org.example.empleado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmpleadoApplication {
 
     public static void main(String[] args) {
