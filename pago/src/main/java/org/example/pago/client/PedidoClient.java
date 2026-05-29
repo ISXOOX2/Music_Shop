5feee852591d2522615105653fde7d8e385440e9
ISXOOX2 <is.cantaro@duocuc.cl>
2026-05-28 23:34:14 -0400

    Remocion de codigo sin uso

--- pago/src/main/java/org/example/pago/client/PedidoClient.java
+++ pago/src/main/java/org/example/pago/client/PedidoClient.java
@@ -4,12 +4,11 @@ import org.example.pago.dto.PedidoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.http.ResponseEntity;
 
 
 @FeignClient(name = "pedido")
 public interface PedidoClient {
 
     @GetMapping("/api/pedidos/{id}")
-    PedidoDTO obtenerPedidoPorId(@PathVariable("id") Integer id);
+    PedidoDTO obtenerPedidoPorId(@PathVariable Integer id);
 }
